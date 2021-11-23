e3bfc508534a7a3385e4d57d70e650c1feab2e09
nhungle <misssg85@gmail.com>
2021-11-23 14:26:10 -0800

    Contact us component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import Header from "./common/HeaderComponent"
 import Footer from "./common/FooterComponent"
 import Home from './HomeComponent';
+import ContactUs from './ContactUsComponent';
 
 export class Main extends Component {
     render() {
@@ -11,6 +12,7 @@ export class Main extends Component {
                 <Header />
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
+                    <Route path="/contactus" element={<ContactUs />}></Route>
                 </Routes>
                 <Footer />
             </div>
